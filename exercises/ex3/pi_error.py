c75a5dfc2c6ce169ee7169b93ba0a092fe96bb96
peiyawang <113616906+peiyawang@users.noreply.github.com>
2022-10-08 11:59:22 +0800

    Update pi_error.py

--- exercises/ex3/pi_error.py
+++ exercises/ex3/pi_error.py
@@ -5,6 +5,6 @@ x,y = np.loadtxt("pi_error.dat",unpack=True)
 plt.plot(np.log10(x),np.log10(y),marker='o', linestyle='dashed')
 plt.xlabel("log(N)")
 plt.ylabel("relative error log(e)")
-plt.savefig("pi_error_AAFO_loge.png")
+plt.savefig("loge_logN.png")
 plt.show()
 
