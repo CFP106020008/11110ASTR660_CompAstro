7a28520f87cb2eb4e815ec277ca353956606792f
CFP106020008 <julius52700@gapp.nthu.edu.tw>
2022-10-24 00:33:18 +0800

    HW2

--- homeworks/HW2/ex3/num.py
+++ homeworks/HW2/ex3/num.py
@@ -1,3 +1,7 @@
+def N(k, em):
+    return (em/2/k)**(-2/(2*(k+1)-1))
+
+
 def etot(N, k, bit):
     if bit == 32:
         em = 1e-7
@@ -7,7 +11,12 @@ def etot(N, k, bit):
         print('Error!')
     return 1/N**k + N**0.5*em
 
-print(etot(1100, 2, 32))
-print(etot(1.7e6, 2, 64))
-print(etot(50, 4, 32))
-print(etot(3000, 4, 64))
+print('{:.2e}'.format(N(2, 1e-7))) 
+print('{:.2e}'.format(N(2, 1e-15)))
+print('{:.2e}'.format(N(4, 1e-7)))
+print('{:.2e}'.format(N(4, 1e-15)))
+
+print('{:.2e}'.format(etot(N(2, 1e-7) , 2, 32)))
+print('{:.2e}'.format(etot(N(2, 1e-15), 2, 64)))
+print('{:.2e}'.format(etot(N(4, 1e-7) , 4, 32)))
+print('{:.2e}'.format(etot(N(4, 1e-15), 4, 64)))
